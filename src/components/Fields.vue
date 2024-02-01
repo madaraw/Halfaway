1d8c47620ef110ec7d37e20967a21d9af8a87ac3
madaraw <rachediwalid@yahoo.fr>
2024-02-01 09:53:13 +0100

    Added a map and fields

---
+++ src/components/Fields.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div>
+            <label for="firstPerson">First Adress: </label>
+            <input id="firstPerson" v-model="firstPerson" type="text">
+        </div>
+        <div>
+            <label for="secondPerson">Second Adress: </label>
+            <input id="secondPerson" v-model="secondPerson" type="text">
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { ref, watch } from 'vue'
+
+    const firstPerson = ref('')
+    const secondPerson = ref('')
+
+    watch(firstPerson, (oldPerson, newPerson) => {
+        console.log(oldPerson, newPerson)
+    })
+
+    watch(secondPerson, (oldSecondPerson, newSecondPerson) => {
+        console.log(oldSecondPerson, newSecondPerson)
+    })
+</script>
---
+++ src/components/Map.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div id="map">
+  </div>
+</template>
+
+
+<script setup>
+  /* eslint-disable */
+  import { onMounted, ref } from 'vue'
+  import { Loader } from "@googlemaps/js-api-loader"
+
+  const map = ref()
+  onMounted(() => {
+    const loader = new Loader({
+      apiKey: "AIzaSyDiNB6iFozzwqoJA9kBQL-0H_U6-szVOW4",
+      version: "weekly",
+    });
+    let googlemaps
+    loader.load().then(async () => {
+      const { Map } = await google.maps.importLibrary("maps");
+
+      googlemaps = new Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+      });
+    });
+  })
+</script>
+
+<style>
+#map {
+  height: 500px;
+}
+</style>
