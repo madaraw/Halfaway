57d127ce7edc70aba67006d037501b7a43994523
madaraw <rachediwalid@yahoo.fr>
2024-02-04 00:17:37 +0100

    Removed watcher made a button to send the parameters to the parent

--- src/components/Fields.vue
+++ src/components/Fields.vue
@@ -1,27 +1,21 @@
 <template>
-    <div class="mb-11">
+    <div>
         <div class="flex flex-col">
             <label for="firstPerson">First Adress: </label>
-            <input class="w-full border-2 border-black rounded-md" id="firstPerson" v-model="firstPerson" type="text">
+            <input class="w-full py-1 border-2 border-black rounded-md" id="firstPerson" v-model="firstPerson" type="text">
         </div>
         <div class="flex flex-col">
             <label for="secondPerson">Second Adress: </label>
-            <input class="w-full border-2 border-black rounded-md" id="secondPerson" v-model="secondPerson" type="text">
+            <input class="w-full py-1 border-2 border-black rounded-md" id="secondPerson" v-model="secondPerson"
+                type="text">
         </div>
+        <button @click="$emit('sendPlaces', [firstPerson, secondPerson])">Check</button>
     </div>
 </template>
 
 <script setup>
-    import { ref, watch } from 'vue'
+    import { ref } from 'vue'
 
     const firstPerson = ref('')
     const secondPerson = ref('')
-
-    watch(firstPerson, (oldPerson, newPerson) => {
-        console.log(oldPerson, newPerson)
-    })
-
-    watch(secondPerson, (oldSecondPerson, newSecondPerson) => {
-        console.log(oldSecondPerson, newSecondPerson)
-    })
 </script>
