b00aca9d92a2068484d388385724527128a8a676
madaraw <rachediwalid@yahoo.fr>
2024-02-04 00:19:32 +0100

    Added a mainvue for everything to be in

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <script setup>
-  import Fields from './components/Fields.vue'
-  import Map from './components/Map.vue'
-  import Results from './components/Results.vue'
+  import MainVue from './components/MainVue.vue'
 </script>
 
 <template>
-  <div class="container mx-auto py-10 px-2 space-y-4">
-    <Fields />
-    <Map />
-    <Results />
-  </div>
+  <MainVue />
 </template>
