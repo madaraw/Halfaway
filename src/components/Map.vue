6a4b6fd319e331b18a04bfd246dab678b8d1f48e
madaraw <rachediwalid@yahoo.fr>
2024-02-04 00:18:25 +0100

    Added a center of map based off of browser localisation, added a function to create markers

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -9,21 +9,39 @@
   import { onMounted, ref } from 'vue'
   import { Loader } from "@googlemaps/js-api-loader"
 
-  const map = ref()
+  const map = ref(null)
+  const placeMarkers = (info1, info2) => {
+    createMarker(info1.places[0].location)
+    createMarker(info2.places[0].location)
+  }
+  let createMarker = null
+  defineExpose({ placeMarkers })
   onMounted(() => {
+    let centerCoords
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        centerCoords = { lat: position.coords.latitude, lng: position.coords.longitude }
+      })
+      console.log("", map.value)
+    }
     const apiKey = import.meta.env.VITE_GOOGLE_API_KEY
     const loader = new Loader({
       apiKey: apiKey,
       version: "weekly",
     });
-    let googlemaps
     loader.load().then(async () => {
       const { Map } = await google.maps.importLibrary("maps");
 
-      googlemaps = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+      map.value = new Map(document.getElementById("map"), {
+        center: centerCoords,
+        zoom: 10,
       });
+      createMarker = (position) => {
+        return new google.maps.Marker({
+          position: { lat: position.latitude, lng: position.longitude },
+          map: map.value,
+        })
+      }
     });
   })
 </script>
