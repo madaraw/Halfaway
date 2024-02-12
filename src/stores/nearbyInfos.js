4ca440704d38a62f87c2237162381fb1e3de3c44
madaraw <rachediwalid@yahoo.fr>
2024-02-12 19:49:37 +0100

    Added a loading variable

--- src/stores/nearbyInfos.js
+++ src/stores/nearbyInfos.js
@@ -4,10 +4,14 @@ import { getMiddleSearch } from "../composables/mapsApiCalls"
 export const useNearbyInfosStore = defineStore('nearbyInfos', {
     state: () => ({
         nearbyInfo: [],
+        nearbyInfoLoading: false,
     }),
     actions: {
         async getNearbyInfo(position, types) {
+            this.nearbyInfo = []
+            this.nearbyInfoLoading = true
             this.nearbyInfo = await getMiddleSearch(position, types)
+            this.nearbyInfoLoading = false
         }
     }
 })
