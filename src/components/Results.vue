fbac89cf0aee4c107f62686dd670f0154fb5a24a
madaraw <rachediwalid@yahoo.fr>
2024-02-14 12:19:25 +0100

    Removed sample text

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -3,12 +3,6 @@
         <h1>{{ place.displayName.text }}</h1><span>{{ place.rating }}</span>
         <p v-if="place.currentOpeningHours">open? {{ place.currentOpeningHours.openNow ? "yes" : "no" }}</p>
     </div>
-    <div>
-        <h2 class="text-xl font-semibold">Simple Text</h2>
-        <p>Simple description: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat cumque id eveniet tempora
-            repellat, dignissimos tempore nobis totam, eligendi, fugit ea dolore quis numquam tenetur officia nulla aperiam
-            expedita deleniti?</p>
-    </div>
 </template>
 <script setup>
     import { computed } from "vue"
